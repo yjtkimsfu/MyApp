938a1f3ca0ac2c3ca545f2ecd3dccf45f18d8581
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-08-12 15:09:51 -0700

    Extended User Model and Add Photo Model then update database using EF

--- MyApp.API/Models/User.cs
+++ MyApp.API/Models/User.cs
@@ -1,10 +1,33 @@
+using System;
+using System.Collections.Generic;
+
 namespace MyApp.API.Models
 {
+    public enum Genders {
+        Other = 0,
+        Male = 1,
+        Female = 2
+    }
     public class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public Genders Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public byte Zipcode { get; set; }
+        public string Country { get; set; }
+        public int PhoneNumbers { get; set; }
+        public string email { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
