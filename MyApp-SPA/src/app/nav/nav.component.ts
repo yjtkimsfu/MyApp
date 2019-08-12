e31bb96b6eae7d92037b9e6643fd3e9cc61e183f
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-08-12 11:53:19 -0700

    Add missing semi colon

--- MyApp-SPA/src/app/nav/nav.component.ts
+++ MyApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(
       () => {
-        this.alertify.success('Logged in successfully')
+        this.alertify.success('Logged in successfully');
       }, error => {
         this.alertify.error(error);
       }
