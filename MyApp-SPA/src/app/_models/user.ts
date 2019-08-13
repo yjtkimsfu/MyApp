4d7b3089c93199e092884fc895431015d1e9d262
YJ Tristan Kim <yjtkimsfu@gmail.com>
2019-08-12 23:09:35 -0700

    Add User and Photo Interfaces

---
+++ MyApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,24 @@
+import { Photo } from './photo';
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    state: string;
+    country: string;
+
+    zipcode?: number;
+    phoneNumber?: number;
+    email?: string;
+
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
